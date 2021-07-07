b12a33ed475ecaed7bc2b6f45e24779e6d688202
Christian daheim <auriocus@gmx.de>
2021-07-07 20:13:00 +0200

    Add build script for CI

---
+++ make-ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+machine=$(uname -sm | tr ' ' '-')
+echo $machine
+
+function download() {
+	url="$1"
+	target="$2"
+	if [ -e "$target" ]; then return; fi
+	wget "$url" -O "$target"
+}
+
+topdir="$(pwd)"
+mkdir -p dependencies
+download https://github.com/BessyHDFViewer/StaticImglibs/releases/download/latest/StaticImglibs_$machine.tar.bz2 dependencies/StaticImglibs.tar.bz2
+
+download https://github.com/auriocus/kbskit/releases/download/latest/kbskit_$machine.tar.bz2 dependencies/kbskit.tar.bz2
+
+( cd dependencies
+  tar xvf StaticImglibs.tar.bz2
+  tar xvf kbskit.tar.bz2 )
+
+
+builddir="$topdir/build"
+distdir="$topdir/dist"
+kbskitdir="$topdir/dependencies/kbskit_$machine"
+tcldir="$kbskitdir/lib"
+staticimglibdir="$topdir/dependencies/StaticImglibs_$machine"
+
+autoconf
+
+./configure LDFLAGS="-L$tcldir" --with-tcl="$tcldir" --with-staticimglibs="$staticimglibdir" --prefix="$builddir" --libdir="$distdir" --exec-prefix="$kbskitdir"
+make
+make install
